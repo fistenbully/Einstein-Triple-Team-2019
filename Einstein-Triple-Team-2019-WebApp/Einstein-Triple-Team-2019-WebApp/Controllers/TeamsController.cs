5d320c1f5c4a3d5dbbdce35bac7950f6fabaa283
Michael Kokonowskyj <mikokono@microsoft.com>
2019-08-20 16:20:22 +0200

    init push

---
+++ Einstein-Triple-Team-2019-WebApp/Einstein-Triple-Team-2019-WebApp/Controllers/TeamsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Einstein_Triple_Team_2019_WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamsController : ControllerBase
+    {
+        private readonly IDbConnection dbConnection;
+
+        public TeamsController(IDbConnection dbConnection)
+        {
+            this.dbConnection = dbConnection;
+        }
+
+        // GET: api/Teams
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var teams = dbConnection.Query("GetAllTeams", CommandType.StoredProcedure);
+
+            return Ok(teams);
+        }
+
+        // GET: api/Teams/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var para = new DynamicParameters();
+            para.Add("@Id", id);
+
+            var team = dbConnection.Query("GetTeamById", param: para, commandType: CommandType.StoredProcedure);
+
+            return Ok(team);
+        }
+    }
+}
